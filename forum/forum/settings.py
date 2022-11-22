dd033c5aeb64ea53557ecf9fb0c7e3f188b53fac
mjpitkin45 <mjpitkin45@gmail.com>
2022-11-22 15:37:20 +0000

    updating repo

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project4_forum',
     }
 }
 
--- forum/users/models.py
+++ forum/users/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    username = models.CharField(unique=True, max_length=30)
-    email = models.EmailField()
-    password = models.CharField(max_length=30)
-    is_admin = models.BooleanField()
+    # username = models.TextField(unique=True)
+    # email = models.EmailField()
+    # password = models.TextField()
+    # is_admin = models.BooleanField(null=True)
+    rating = models.CharField(max_length=10)
